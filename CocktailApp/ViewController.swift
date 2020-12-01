d73e9fada111f3fdc06c139ad8fc2021a9002898
Shauni Vansteyvoort <shaunivansteyvoort@gmail.com>
2020-12-01 15:15:28 +0100

    Initial Commit

---
+++ CocktailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailApp
+//
+//  Created by shauni vansteyvoort on 01/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
